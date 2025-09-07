87606d37267c7904b645dad46099a741fcc20c2b
pranathidevaki <pdevakimf@gmail.com>
2025-09-07 16:13:21 +0530

    backend successful

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/login' element={<LoginPage/>} />
+            <Route path='user/me' element={<Profile/>} />
             <Route element={<PrivateRoute/>}>
 
             </Route>
